77999266108950dad27dddc4fd210ea98807b7d1
Enzo Givernaud <egd@sahar.fr>
2026-04-07 12:17:21 +0200

    delete useless file

--- drizzle_fix.sql
+++
@@ -1,6 +0,0 @@
-TRUNCATE "__drizzle_migrations" RESTART IDENTITY;
-INSERT INTO "__drizzle_migrations" (hash, created_at) VALUES
-('undefined', undefined),
-('undefined', undefined),
-('undefined', undefined),
-('undefined', undefined);
